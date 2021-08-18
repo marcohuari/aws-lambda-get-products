b436d56a35673f7c199ae55d2b366583ee4e519e
Duvan Saenz <duvanbradbrid@gmail.com>
2021-08-18 02:05:08 -0500

    Get products

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-export const getProducts = (event, context, callback) => {
-  return 'Hello World';
+'use strict';
+
+module.exports.getProducts = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: products(),
+    };
+    return response;
 };
+
+
+const products = function () {
+    return [
+        {
+            id: 1,
+            name: 'Lomo Saltado',
+            price: 12.5
+        },
+        {
+            id: 2,
+            name: 'Arroz a la cubana',
+            price: 8.5
+        }];
+}
